6e111b995d7780f7eb3a17a15ba82de2b9d55499
joel <joel.ortizlancharro@gmail.com>
2026-05-11 16:24:27 +0200

    feature: inicialitzacio BD + menu

--- src/App.java
+++ src/App.java
@@ -1,16 +1,63 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import utils.ConnexioBD;
 
 public class App {
     static Connection conn;
     static ConnexioBD conexio=null;
-    public static void main(String[] args) throws Exception {
-        App p = new App();
-        p.principal();
+    Scanner sc = new Scanner(System.in);
+      
+    ControlJSON json = new ControlJSON();
 
-    }
-    public void principal() {
+
+    public static void main(String[] args) throws Exception {
         conexio = new ConnexioBD("tpv_botiga");
         conexio.establirConexio();
+        App p = new App();
+        p.principal();
     }
+    public void principal(){
+        menu();
+    }
+
+    public void menu(){
+        System.out.println("==============================");
+        System.out.println("Benvingut a la nostre tenda!!!");
+        System.out.println("==============================");
+        System.out.println("1. Inicialitzar BD, JSON a Base de dades.");
+        System.out.print("Que vols fer?");
+        int num = sc.nextInt(); 
+        opcions(num); //Aqui cridem el switch
+    }
+
+    public void opcions(int num){ //Aixo es el switch
+        switch (num) {
+            case 1:
+                inicialitzarBD();
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    public void inicialitzarBD(){
+        ArrayList<Producte> productes = json.llegirProductes();
+       
+        for(int j = 0; j < productes.size(); j++){
+            if(productes.get(j) instanceof Pantalo){
+                    Pantalo p = (Pantalo)productes.get(j);
+                     conexio.insertArticle(p.getId(), p.getNom(), p.getFamilia(), p.getTallaCintura(), p.getLlargadaCamal(), p.getPreuBase(), p.getIVA(), p.getStock());
+        
+                    }
+                    else{
+                        Camisa c = (Camisa)productes.get(j);
+                         conexio.insertArticle(c.getId(), c.getNom(), c.getFamilia(), c.getTallaColl(), c.getAmpladaPit(), c.getPreuBase(), c.getIVA(), c.getStock());
+        
+                    }
+
+    }
+}
 }
