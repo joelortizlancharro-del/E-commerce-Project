ce6c13e68d2eb5458b79c37804a9393f89dbc75f
joel <joel.ortizlancharro@gmail.com>
2026-05-12 16:54:03 +0200

    fix: sprint 1 + BD v2

--- src/utils/ConnexioBD.java
+++ src/utils/ConnexioBD.java
@@ -5,24 +5,15 @@ import java.sql.*;
 public class ConnexioBD{
     
     static String URL;
-<<<<<<< HEAD
     static final String USER = "root";/*System.getenv("DB_USER");*/
     static final String PASSWORD = "Programicion24!";/*System.getenv("DB_PASSWORD"); */
-=======
-    static final String USER = "root";
-    static final String PASSWORD = "";
->>>>>>> fix/BD
     public static Connection conn;
     //
     public ConnexioBD(String nomBD){
         URL= "jdbc:mysql://127.0.0.1:3306/" + nomBD + "?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
     }
 
-<<<<<<< HEAD
-    public boolean establirConexio(){
-=======
     public  boolean establirConexio(){
->>>>>>> fix/BD
         boolean result = true;
         try {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -30,10 +21,7 @@ public class ConnexioBD{
             e.printStackTrace();
             result = false;
         }
-<<<<<<< HEAD
         
-=======
->>>>>>> fix/BD
         return result;
     }
     
@@ -82,42 +70,6 @@ public class ConnexioBD{
             }
 
             PreparedStatement ps = conn.prepareStatement(sql);
-<<<<<<< HEAD
-            ps.setInt(1, id);
-            rs = stmt.executeQuery(sql);
-         }catch(Exception e){
-                e.printStackTrace();
-            }
-        return rs;
-    }
-    public ResultSet updateArticle(int id, String nom, String familia, int tallaColl_cintura, int ampladaPit_llargadaCamal, double preu_base, int iva, int stock){
-        ResultSet rs=null;
-        try{
-                Statement stmt = conn.createStatement();
-                String sql = "UPDATE articles nom = ? , familia = ?, ? = ?, ? = ?, preu_base = ?, iva = ?, stock = ? WHERE id = ?;";
-                
-                PreparedStatement ps = conn.prepareStatement(sql);
-                if(familia.equals("camisa")){
-                    ps.setString(3, "talla_coll");
-                    ps.setString(5, "amplada_pit");
-                }else {
-                    ps.setString(3, "talla_cintura");
-                    ps.setString(5, "llargada_camal");
-                }
-                ps.setString(1, nom);
-                ps.setString(2, familia);
-                ps.setInt(4, tallaColl_cintura);
-                ps.setInt(6, ampladaPit_llargadaCamal);
-                ps.setDouble(7, preu_base);
-                ps.setInt(8, iva);
-                ps.setInt(9, stock);
-                ps.setInt(10, id);
-                rs = stmt.executeQuery(sql);
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        return rs;
-=======
 
             ps.setInt(1, id);
             ps.setString(2, nom);
@@ -153,6 +105,5 @@ public class ConnexioBD{
         } catch(Exception e){
             e.printStackTrace();
         }
->>>>>>> fix/BD
     }
 }
