5bf9b9baf2c8d48f6320371b0b11f0f94d62f0fd
ACA <alberkstyon@gmail.com>
2026-05-21 16:55:44 +0200

    feature gestió d' stock

--- src/App.java
+++ src/App.java
@@ -6,65 +6,87 @@ import utils.ConnexioBD;
 
 public class App {
     static Connection conn;
-    static ConnexioBD conexio=null;
+    static ConnexioBD conexio = null;
     Scanner sc = new Scanner(System.in);
-      
-    ControlJSON json = new ControlJSON();
 
+    ControlJSON json = new ControlJSON();
 
     public static void main(String[] args) throws Exception {
         conexio = new ConnexioBD("tpv_botiga");
-      if (conexio.establirConexio()){
-        System.out.println("Conectat");
-      }
-      else{
-        System.out.println("Error");
-      }
+        if (conexio.establirConexio()) {
+            System.out.println("Conectat");
+        } else {
+            System.out.println("Error");
+        }
         App p = new App();
         p.principal();
     }
-    public void principal(){
+
+    public void principal() {
         menu();
     }
 
-    public void menu(){
+    public void menu() {
         System.out.println("==============================");
         System.out.println("Benvingut a la nostre tenda!!!");
         System.out.println("==============================");
         System.out.println("1. Inicialitzar BD, JSON a Base de dades.");
         System.out.print("Que vols fer?");
-        int num = sc.nextInt(); 
-        opcions(num); //Aqui cridem el switch
+        int num = sc.nextInt();
+        opcions(num); // Aqui cridem el switch
     }
 
-    public void opcions(int num){ //Aixo es el switch
+    public void opcions(int num) { // Aixo es el switch
 
         switch (num) {
             case 1:
                 inicialitzarBD();
                 break;
-        
+
             default:
                 break;
         }
     }
 
-    public void inicialitzarBD(){
-        
-        ArrayList<Producte> productes = json.llegirProductes();
-        
-        for(int j = 0; j < productes.size(); j++){
-            if(productes.get(j) instanceof Pantalo){
-                    Pantalo p = (Pantalo)productes.get(j);
-                     conexio.insertArticle(p.getId(), p.getNom(), p.getFamilia(), p.getTallaCintura(), p.getLlargadaCamal(), p.getPreuBase(), p.getIVA(), p.getStock());
-        
-                    }
-                    else{
-                        Camisa c = (Camisa)productes.get(j);
-                         conexio.insertArticle(c.getId(), c.getNom(), c.getFamilia(), c.getTallaColl(), c.getAmpladaPit(), c.getPreuBase(), c.getIVA(), c.getStock());
-        
-                    }
+    public void inicialitzarBD() {
 
+        ArrayList<Producte> productes = json.llegirProductes();
+
+        for (int j = 0; j < productes.size(); j++) {
+            if (productes.get(j) instanceof Pantalo) {
+                Pantalo p = (Pantalo) productes.get(j);
+                conexio.insertArticle(p.getId(), p.getNom(), p.getFamilia(), p.getTallaCintura(), p.getLlargadaCamal(),
+                        p.getPreuBase(), p.getIVA(), p.getStock());
+
+            } else {
+                Camisa c = (Camisa) productes.get(j);
+                conexio.insertArticle(c.getId(), c.getNom(), c.getFamilia(), c.getTallaColl(), c.getAmpladaPit(),
+                        c.getPreuBase(), c.getIVA(), c.getStock());
+
+            }
+
+        }
     }
+
+    public boolean checkStock(int quantitat, int idArticle) {
+
+    boolean result = false;
+
+    try {
+        ResultSet rs = conexio.selectArticlesById(idArticle);
+
+        if (rs.next()) {
+
+            int stockActual = rs.getInt("stock");
+
+            result = stockActual >= quantitat;
+        }
+
+    } catch (SQLException e) {
+        System.err.println("Error comprovant stock: " + e.getMessage());
+        e.printStackTrace();
+    }
+
+    return result;
 }
 }
